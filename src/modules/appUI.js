c0eaa89083e0818320664bc8a935c2b005a83ccf
asanchy <sanchyclips@gmail.com>
2022-02-01 13:40:23 +0200

    Add like icon and comments button

--- src/modules/appUI.js
+++ src/modules/appUI.js
@@ -1,19 +1,30 @@
 import getSeries from './series.js';
 import getMovies from './movies.js';
 
+const filmTemplate = (info) => {
+  return `
+    <li>
+      <div class="film-image">
+        <img src=${info.image}>
+      </div>
+      <div class="film-info">
+        <h1 class="film-title">${info.name}</h1>
+        <div>
+          <i class="far fa-heart"></i>
+        </div>
+      </div>
+      <button type="button" class="comments">Comments</button>
+    </li>
+  `;
+}
+
+
 const displayMovies = () => {
   const filmsContainer = document.querySelector('.films');
   filmsContainer.innerHTML = '';
   getMovies().forEach(async (movie) => {
     const movieData = await movie;
-    filmsContainer.innerHTML += `
-    <li>
-      <div class="film-image">
-        <img src=${movieData.image}>
-      </div>
-      <h1>${movieData.name}</h1>
-    </li>
-    `;
+    filmsContainer.innerHTML += filmTemplate(movieData);
   });
 };
 
@@ -22,14 +33,7 @@ const displaySeries = () => {
   filmsContainer.innerHTML = '';
   getSeries().forEach(async (movie) => {
     const movieData = await movie;
-    filmsContainer.innerHTML += `
-    <li>
-      <div class="film-image">
-        <img src=${movieData.image}>
-      </div>
-      <h1>${movieData.name}</h1>
-    </li>
-    `;
+    filmsContainer.innerHTML += filmTemplate(movieData);
   });
 };
 
